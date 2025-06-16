07c96e4fe9c62d6e34d35a55921f9c834236f159
tofashish527 <tofashish000@gmail.com>
2025-06-16 14:18:02 +0600

    my application data fetched

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
     await client.connect();
 
     const jobsCollection=client.db('Job-Portal').collection('jobs');
+    const applicationsCollection=client.db('Job-Portal').collection('applications');
 
     app.get('/jobs',async(req,res)=>{
          const cursor=await jobsCollection.find();
@@ -44,6 +45,31 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/applications',async(req,res)=>{
+      const email=req.query.email;
+      const query={
+        applicant:email
+      }
+      const result=await applicationsCollection.find(query).toArray();
+
+      //bad way to aggregate data
+      for(const application of result)
+      {
+        const jobId=application.jobId;
+        const jobQuery={_id:new ObjectId(jobId)}
+        const job=await jobsCollection.findOne(jobQuery);
+        application.company=job.company;
+        application.title=job.title;
+        application.company_logo=job.company_logo
+      }
+      res.send(result);
+    })
+
+    app.post('/applications',async(req,res)=>{
+        const application=req.body;
+        const result=await applicationsCollection.insertOne(application);
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
